48e3217fbf9de8f12410d95e002dc0c55b44d275
liubchi <liutsar346@gmail.com>
2025-12-09 08:24:48 +0100

    Solve Simula's Test

---
+++ Part02/Level16-Enumerations/SimulaTest/Program.cs
@@ -0,0 +1,29 @@
+﻿ChestState currentState = ChestState.Closed;
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+
+    string command = Console.ReadLine();
+    currentState = PerformAction(command, currentState);
+}
+
+ChestState PerformAction(string command, ChestState currentState)
+{
+    if (command == "close" && currentState == ChestState.Open)
+        return ChestState.Closed;
+    if (command == "lock" && currentState == ChestState.Closed)
+        return ChestState.Locked;
+    if (command == "unlock" && currentState == ChestState.Locked)
+        return ChestState.Closed;
+    if (command == "open" && currentState == ChestState.Closed)
+        return ChestState.Open;
+    return currentState;
+}
+
+enum ChestState
+{
+    Open,
+    Closed,
+    Locked
+}
